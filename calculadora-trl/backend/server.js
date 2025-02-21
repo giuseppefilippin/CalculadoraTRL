edb6d9e9cfc50229111c5986ddb3e60898c79d1b
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-02-21 10:51:42 -0300

    testing servers

---
+++ calculadora-trl/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor da Calculadora TRL rodando!");
+});
+
+// Iniciando o servidor na porta 5000
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
